e1cbc53960a6469cef56e789b6c352bb0e09b7e6
August Ye <augustye@gmail.com>
2020-03-02 12:35:11 +0800

    add fps

--- test_camera.py
+++ test_camera.py
@@ -9,18 +9,20 @@ model_path = "model/multi_person_mobilenet_v1_075_float.tflite"
 estimator = Estimator(16,model_path,0.5,5,50,1)
 cam = cv2.VideoCapture(0)
 
+start_time = time.time()
+frames = 0
 while True:
-
     ret_val, image = cam.read()
-
     image = crop_image(image)
     (fac,fitted_img) = resize_img(image)
-
     poses = estimator.process_img(fitted_img,fac)
-
     estimator.draw_poses(poses,image,-100)
-
     cv2.imshow('cam_demo',image)
+    frames += 1
+
+    if frames%100 == 0:
+        print("frames:", frames, ", fps:", frames/(time.time()-start_time))
+
     if cv2.waitKey(1) == 27:
         break
 
